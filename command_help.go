2db7953c8faeb04aea7da6499fdd89d37a165313
ryderwsos <ryder0019@gmail.com>
2025-03-21 21:37:12 -0400

    added exit and help command

---
+++ command_help.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("-----------------------------------------------------")
+	fmt.Println("Pokedex commands, type first word:")
+	for _, i := range getCommand() {
+		fmt.Printf("\"%s\" - %s\n", i.name, i.description)
+	}
+	fmt.Println("-----------------------------------------------------")
+	return nil
+}
--- repl.go
+++ repl.go
@@ -19,7 +19,17 @@ func startRepl() {
 
 		words := cleanInput(userInput)
 
-		fmt.Printf("Your command was: %s\n", strings.ToLower(words[0]))
+		command, exist := getCommand()[words[0]]
+		if exist {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("unkown command")
+			continue
+		}
 	}
 }
 
@@ -27,4 +37,25 @@ func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	textArr := strings.Fields(lowerText)
 	return textArr
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommand() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Display all console commands",
+			callback:    commandHelp,
+		},
+	}
 }
